2a46aec715991470fdbd664acbc54ad909838bbc
中村直人 <nakamuranaoto@MacBook-Pro-5.local>
2021-01-12 20:54:43 +0900

    不要変数削除

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import ParentForm from './ParentForm';
 import React from 'react';
 import './App.css';
 
+export const ChildIdContext = React.createContext();
+
 function App() {
   const [text, setText] = React.useState([{id: 1, parentText: 'test'}]);
-  const [parentId, setParentId] = React.useState(0);
   const [parentText, setParentText] = React.useState('');
 
   return (
     <div className="App">
       TODOフォーム
+      <ChildIdContext.Provider value={100}>
         <ParentForm 
           text={text} 
           setText={setText} 
           parentText={parentText} 
           setParentText={setParentText}
-          parentId={parentId}
-          setParentId={setParentId}
         />
+      </ChildIdContext.Provider>
     </div>
   );
 }
