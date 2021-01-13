9a3732f3c8cecf6b3ac36defdf6cbefae5ae3227
中村直人 <nakamuranaoto@MacBook-Pro-5.local>
2021-01-13 22:53:33 +0900

    子コンポーネントの登録処理を追加

--- src/ChildForm.js
+++ src/ChildForm.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 const ChildForm = (props) => {
+  const [text, setText] = React.useState([]);
+  const [childText, setChildText] = React.useState('');
+
+  const handleChange = e => {setChildText(e.target.value)}
+  const handleClick = () => {
+    setText([...text, {
+      childText: childText,
+    }])
+    setChildText('')
+  }
 
   return (
     <div>
-      <input></input>
-      <button>{props.parentId}の子追加</button>
+      <input value={childText} onChange={handleChange}></input>
+      <button onClick={handleClick}>{props.parentId}の子追加</button>
+      {text.map((item, i) => (
+         <div key={i}>
+          <li>
+          <b>{item.childText}</b>
+          </li>
+         </div>
+      ))}
     </div>
   );
 }
